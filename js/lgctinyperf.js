3162137740fca519624419c9712c1a53746843fd
ARZUMATA <arzumata@gmail.com>
2026-03-23 02:57:53 +0300

    collapsed nodes now drawn right

--- js/lgctinyperf.js
+++ js/lgctinyperf.js
@@ -60,24 +60,32 @@ app.registerExtension({
         LGC.prototype.drawNode = function(node, ctx) {
             // We use the 'isGhosting' flag set in the draw loop above
             if (isGhosting) {
-                const [w, h] = node.size;
+                let [w, h] = node.renderingSize;
+                
+                // We also have title height as it's positioned above the node body
+                const titleHeight = LiteGraph?.NODE_TITLE_HEIGHT || 30;
+                h = node.flags?.collapsed ? titleHeight : h + titleHeight;
+                
+                if (node.flags?.collapsed) {
+                    w = node._collapsed_width || LiteGraph?.NODE_COLLAPSED_WIDTH || 100;
+                }
+
                 const color = node.color || "#333";
 
-                // Simple Box
+                // Simple Box - draw at proper position with title height in mind
                 ctx.fillStyle = "#1a1a1a";
                 ctx.beginPath();
-                ctx.roundRect(0, 0, w, h, 4);
+                ctx.roundRect(0, -titleHeight, w, h, 4);
                 ctx.fill();
 
                 // Colored Header
                 ctx.fillStyle = color;
                 ctx.beginPath();
-                ctx.roundRect(0, 0, w, 22, [4, 4, 0, 0]); 
-                // ctx.roundRect(0, 0, w, 22); 
+                ctx.roundRect(0, -titleHeight, w, titleHeight, [4, 4, 0, 0]);
                 ctx.fill();
 
                 ctx.strokeStyle = color;
-                ctx.strokeRect(0, 0, w, h);
+                ctx.strokeRect(0, -titleHeight, w, h);
                 
                 return; // SKIP everything else
             }
